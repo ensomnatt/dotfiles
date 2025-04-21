0dfcb86e96d6eb183a5a1778f98971eae83767f3
ensomnatt <ensomnatt@protonmail.com>
2025-04-21 19:01:20 +0300

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,15 +6,48 @@ config.font = wezterm.font("Mononoki Nerd Font")
 config.font_size = 19
 
 config.enable_tab_bar = false
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
 
 config.window_decorations = "NONE"
 config.window_background_opacity = 0.9
 
 config.window_padding = {
-	left = 20,
-	right = 20,
-	top = 20,
-	bottom = 15,
+	left = 22,
+	right = 22,
+	top = 22,
+	bottom = 5,
+}
+
+config.window_frame = {
+  font = wezterm.font { family = "Mononoki Nerd Font", weight = "Bold" },
+
+  font_size = 15.0,
+
+  active_titlebar_bg = "rgba(0, 0, 0, 0.75)",
+}
+
+config.colors = {
+  cursor_bg = "#ffffff",
+
+  tab_bar = {
+    background = "rgba(0, 0, 0, 0.8)",
+
+    active_tab = {
+      bg_color = "rgba(0, 0, 0, 0.8)",
+      fg_color = "white",
+    },
+
+    inactive_tab = {
+      bg_color = "rgba(0, 0, 0, 0.8)",
+      fg_color = "white",
+    },
+
+    new_tab = {
+      bg_color = "rgba(0, 0, 0, 0.8)",
+      fg_color = "#6d000f",
+    },
+  }
 }
 
 return config
