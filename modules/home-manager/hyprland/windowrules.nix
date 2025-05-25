016e1c28335de78c6ff5453bbdcb12e883904b6c
ensomnatt <ensomnatt@protonmail.com>
2025-05-25 03:54:16 +0300

    update

--- modules/home-manager/hyprland/windowrules.nix
+++ modules/home-manager/hyprland/windowrules.nix
@@ -4,8 +4,8 @@
   wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
       "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-      "opacity 1.0 1.0, class:^(firefox)$"
-      "opacity 1.0 override, title: (.*YouTube.*)"
+      "opacity 0.9, class:^(rofi)$"
+      "rounded 18, class:^(rofi)$"
       "opacity 0.9, class:^(org.wezfurlong.wezterm)$"
     ];
   };
